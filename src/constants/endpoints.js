3523b60a875d7fa1135dffe70e6ea7208dc10031
Lucio Henrique <luciolhl@hotmail.com>
2022-06-23 23:38:50 -0300

    Correction

--- src/constants/endpoints.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL =
-  "https://cors-anywhere.herokuapp.com/https://headstarttools.cs.blip.ai/";
-export const UPDATE_SCRIPTS = "/api/v2/HeadstartTemplate";
-export const COPY_FLOW = "/api/v2/HeadstartTemplate/copy-flow-and-publish";
--- src/services/local-storage-service.js
+++
@@ -1,8 +0,0 @@
-export const getBot = (itemKey) => {
-  let item = localStorage.getItem(itemKey);
-  return JSON.parse(item);
-};
-
-export const setBot = (itemKey, itemValue) => {
-  localStorage.setItem(itemKey, JSON.stringify(itemValue));
-};
